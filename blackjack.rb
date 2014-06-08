2c58e51bdd1404a199adc2c2b2157f78eb7c71c4
maebeale <maebeale@gmail.com>
2014-06-08 13:23:51 -0400

    added koans

---
+++ blackjack.rb
@@ -0,0 +1,24 @@
+Class Blackjackgame
+  def initialize
+
+  end
+
+  def cards
+
+  end
+
+  def playerstash
+    #iterates
+  end
+  
+  def playerbet
+
+  end
+
+  def dealerstash
+
+  end
+
+  def dealerbet
+
+  end
---
+++ objectid.rb
@@ -0,0 +1,5 @@
+def df
+  assert_equal 201, 100.object_id
+  puts "true"
+  puts 100.object_id.to_s
+end
